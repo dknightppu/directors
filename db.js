8f2ed438eb70cd1262beed265cc45061300fb634
Daontay Knight <daont@MacBook-Pro-2.local>
2025-03-31 15:09:34 -0400

    bye

--- db.js
+++ db.js
@@ -8,7 +8,7 @@ const pool = new Pool({
     //host: "localhost",
     host: process.env["dpg-cvl165odl3ps738e8uo0-a"],
     //database: "directors",
-    database: "directors",
+    database: "directors-1",
     //password: "Orderofblue33!",
     password: "Hr32a2zj9jdlStg0L0cq9r1b3z4OhhJY",
     port: 5432,
